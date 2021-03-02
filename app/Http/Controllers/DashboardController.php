2d3d2569c137a01b20bb53a02590bb942dea67fb
kaotu <kao-tu@hotmail.co.th>
2021-03-02 17:04:19 +0700

    Add get method for chart dashboard feature 🌚

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,4 +20,25 @@ class DashboardController extends Controller
         $stats = $this->dashboard->getStats();
         return response()->json($stats, 200);
     }
+
+    public function getCompanyTypes(Request $request)
+    {
+        $company_types = $request->all();
+        $company_types = $this->dashboard->getCompanyTypes();
+        return response()->json($company_types, 200);
+    }
+
+    public function getStudentJobPositions(Request $request)
+    {
+        $student_job_positions = $request->all();
+        $student_job_positions = $this->dashboard->getStudentJobPositions();
+        return response()->json($student_job_positions, 200);
+    }
+
+    public function getAnnouncementJobPositions(Request $request)
+    {
+        $announcement_job_position = $request->all();
+        $announcement_job_position = $this->dashboard->getAnnouncementJobPositions();
+        return response()->json($announcement_job_position, 200);
+    }
 }
