80b3450a3302744eb5c41e956f0d8c406d1e76d4
kaotu <kao-tu@hotmail.co.th>
2021-03-04 14:56:29 +0700

    Add method get for get role_permissions 🌚

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -5,20 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller as Controller;
+use App\Repositories\RolePermissionRepositoryInterface;
 use App\Repositories\RoleRepositoryInterface;
 
-class RoleController extends Controller 
+class RoleController extends Controller
 {
     private $role;
+    private $role_permission;
 
-    public function __construct(RoleRepositoryInterface $roleRepo)
+    public function __construct(RoleRepositoryInterface $roleRepo, RolePermissionRepositoryInterface $role_permission_repo)
     {
         $this->role = $roleRepo;
+        $this->role_permission = $role_permission_repo;
     }
 
     public function get(Request $request)
     {
         $jobs = $this->role->getRoles();
         return response()->json($jobs, 200);
+    }
+
+    public function getRolePermissions(Request $request)
+    {
+        $role_permission = $request->all();
+        $role_permission = $this->role_permission->getRolePermissions();
+        return response()->json($role_permission, 200);
     }
 }
