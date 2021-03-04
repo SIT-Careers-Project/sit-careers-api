3c1e6f6d2d36bba0d227d84e366bb789abe84572
kaotu <kao-tu@hotmail.co.th>
2021-03-04 14:57:07 +0700

    Add ORM function get role_permissions 🌚

--- app/Repositories/RolePermissionRepositoryInterface.php
+++ app/Repositories/RolePermissionRepositoryInterface.php
@@ -7,4 +7,5 @@ interface RolePermissionRepositoryInterface
     public function getUserRolePermissions($user_id);
     public function getRolePermissions();
     public function getRolePermissionsByUserId($user_id);
+    public function getRolePermissions();
 }
