74dc4b34aa50932f5d2bca949d3cf2cad211e028
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-03-05 20:38:42 +0700

    :white_check_mark: Update test again 🌻

--- tests/Unit/ApplicationTest.php
+++ tests/Unit/ApplicationTest.php
@@ -298,7 +298,7 @@ class ApplicationTest extends TestCase
 
         $mockData = [
             'announcement_id' => $announcement_arr,
-            'my_user_id' => $user->user_id,
+            'student_id' => $user->user_id,
             'application_date' => '2021-02-04',
             'status' => '-',
             'note' => '-',
