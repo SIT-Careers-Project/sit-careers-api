49f5ccc4d3e1431bda021cf381c8f271465b0398
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-03-17 17:37:51 +0700

    :zap: Update respository  of role for check permision🌻

--- app/Repositories/RolePermissionRepository.php
+++ app/Repositories/RolePermissionRepository.php
@@ -6,12 +6,16 @@ use App\Models\RolePermission;
 
 class  RolePermissionRepository implements RolePermissionRepositoryInterface
 {
-    public function getUserRolePermissions($user_id)
+    public function getUserRolePermissions($data, $permission)
     {
         $user_role_permission = RolePermission::join('roles', 'roles.role_id', '=', 'role_permissions.role_id')
                                 ->join('permissions', 'permissions.permission_id', '=', 'role_permissions.permission_id')
                                 ->join('users', 'users.role_id', '=', 'role_permissions.role_id')
-                                ->where('users.user_id', $user_id)->first();
+                                ->where([
+                                    ['users.user_id', '=', $data['my_user_id']],
+                                    ['roles.role_id', '=', $data['my_role_id']],
+                                    ['permissions.permission_name', $permission]
+                                ])->get();
         return $user_role_permission;
     }
 
