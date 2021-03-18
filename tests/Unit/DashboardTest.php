c0c674094f7c4cb9bb7daa70615231b6cb6e5742
kaotu <kao-tu@hotmail.co.th>
2021-03-18 10:20:21 +0700

    ✅ Add export companies report success case 🌚

--- tests/Unit/DashboardTest.php
+++ tests/Unit/DashboardTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\Traits\CompaniesExport;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Maatwebsite\Excel\Facades\Excel;
 use Tests\TestCase;
 
 class DashboardTest extends TestCase
@@ -29,5 +32,26 @@ class DashboardTest extends TestCase
     public function test_get_announcement_job_position_success_should_return_status_200()
     {
         $this->get('api/dashboard/announcements/job-positions')->assertStatus(200);
+    }
+
+    public function test_export_companies_report_with_storage_success_should_return_true()
+    {
+        Excel::fake();
+
+        $data = [
+            'start_date' => '2021-02-19',
+            'end_date' => '2021-02-20'
+        ];
+
+        $response = $this->call('GET', 'api/dashboard/companies/export', $data);
+
+        $file_name = 'companies'. '_' . Carbon::now()->format('Y-m-d-H-i-s') . '.csv';
+        $path = '/reports/companies/';
+        $path_file_name = $path.$file_name;
+
+        Excel::assertStored($path_file_name, 'minio', function(CompaniesExport $export)
+        {
+            return true;
+        });
     }
 }
