84d2a060595367d359d7dc0771c855705a16c47d
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-04-14 14:37:13 +0700

    :lipstick: Adjust verication mail formatt follow by document in google word sheet 🌻

--- resources/views/emails/verify_email.blade.php
+++ resources/views/emails/verify_email.blade.php
@@ -24,26 +24,25 @@
       <p>เรียนเจ้าของ E-mail: {{ $email }}</p>
       @if ($company_name_th)
         <p class="line-height-6">
-          บริษัท {{ $company_name_th }} - {{ $company_name_en }} ได้เพิ่มคุณเป็นผู้ใช้งานในระบบ 
-          SIT-Industry Collaboration Service System สำเร็จ กรุณาไปยัง
+          {{ $company_name_th }} - {{ $company_name_en }} ได้เพิ่มคุณเป็นผู้ใช้งานในระบบ 
+          SIT Career Center สำเร็จ กรุณาไปยัง
           <a href="{{ $url }}/login/verification?urlVerify= {{ $code_verify }}">
             {{ $url }}/login/verification?urlVerify= {{ $code_verify }}
           </a> เพื่อเข้าไปทำการตั้งรหัสผ่านใหม่ของคุณ
         </p>       
       @else
         <p class="line-height-6">
-          คุณถูกเพิ่มให้เป็นผู้ใช้งานในระบบ SIT-Industry Collaboration Service System กรุณาไปยัง
+          คุณถูกเพิ่มให้เป็นผู้ใช้งานในระบบ SIT Career Center กรุณาไปยัง
           <a href="{{ $url }}/login/verification?urlVerify= {{ $code_verify }}">
             {{ $url }}/login/verification?urlVerify= {{ $code_verify }}
           </a> เพื่อเข้าไปทำการตั้งรหัสผ่านใหม่ของคุณ
         </p> 
       @endif
-      <p>หากมีข้อสงสัยหรือสอบถามกรุณาติดต่อ 66 2470 9850 หรือ 
-        <a href="https://www.facebook.com/SIT.Family" class="">SIT KMUTT</a>
-        Facebook Fanpage
+      <p>หากคุณมีข้อสงสัยหรือต้องการติดต่อสอบถามเพิ่มเติม สามารถติดต่อได้ที่ คุณรุ่งโรจน์ ขวัญโกมล Tel: 66 2470 9850 E-mail: 
+        <a href="mailto:rungroj@sit.kmutt.ac.th">rungroj@sit.kmutt.ac.th</a>
       </p>
       <hr style="margin-top: 6px; margin-bottom: 3px;"/>
-      <p class="text-sm">© SIT-Industry Collaboration Service System</p>
+      <p class="text-sm">© SIT Career Center</p>
     </div>
   </body>
 </html>
