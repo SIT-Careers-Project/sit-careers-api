18bec36bba27a1caee7437453fd45d914780b4fa
kaotu <kao-tu@hotmail.co.th>
2021-04-15 20:25:02 +0700

    🐛 Remove unique rule 🌚

--- app/Http/RulesValidation/AnnouncementResumeRules.php
+++ app/Http/RulesValidation/AnnouncementResumeRules.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\RulesValidation;
 
+use Illuminate\Validation\Rule;
+
 trait AnnouncementResumeRules
 {
     private $ruleCreateAnnouncementResume = [
-        'announcement_id' => 'required|string|unique:announcement_resumes',
+        'announcement_id' => 'required|string',
         'resume_id' => 'required|string',
         'status' => 'nullable|string',
         'note' => 'nullable|string',
