1a15ea989745eec41d15e0118488e670a2b268a7
kaotu <kao-tu@hotmail.co.th>
2021-04-30 14:51:32 +0700

    🚑 Hotfix get announcement resume data 🌚

--- app/Repositories/AnnouncementResumeRepository.php
+++ app/Repositories/AnnouncementResumeRepository.php
@@ -40,7 +40,7 @@ class AnnouncementResumeRepository implements AnnouncementResumeRepositoryInterf
             $announcement_resumes = AnnouncementResume::join('resumes', 'resumes.resume_id', '=', 'announcement_resumes.resume_id')
                 ->join('announcements', 'announcements.announcement_id', '=', 'announcement_resumes.announcement_id')
                 ->join('companies', 'companies.company_id', '=', 'announcements.company_id')
-                ->select('companies.company_name_th', 'announcements.announcement_title', 'resumes.*')
+                ->select('companies.company_name_th', 'announcements.announcement_title', 'resumes.*', 'announcement_resumes.*')
                 ->where('companies.company_id', '=', $company[0]->company_id)
                 ->get();
             return $announcement_resumes;
@@ -79,7 +79,7 @@ class AnnouncementResumeRepository implements AnnouncementResumeRepositoryInterf
             $announcement_resumes = AnnouncementResume::join('resumes', 'resumes.resume_id', '=', 'announcement_resumes.resume_id')
                 ->join('announcements', 'announcements.announcement_id', '=', 'announcement_resumes.announcement_id')
                 ->join('companies', 'companies.company_id', '=', 'announcements.company_id')
-                ->select('companies.company_name_th', 'announcements.announcement_title', 'resumes.*')
+                ->select('companies.company_name_th', 'announcements.announcement_title', 'resumes.*', 'announcement_resumes.*')
                 ->where('companies.company_id', '=', $company[0]->company_id)
                 ->where('announcement_resumes.announcement_resume_id', $announcement_resume_id)
                 ->get();
@@ -96,7 +96,8 @@ class AnnouncementResumeRepository implements AnnouncementResumeRepositoryInterf
             ->join('companies', 'companies.company_id', '=', 'announcements.company_id')
             ->where('resumes.student_id', $data['my_user_id'])
             ->where('announcement_resumes.announcement_resume_id', $announcement_resume_id)
-            ->select('companies.company_name_th', 'announcements.announcement_title', 'resumes.*')
+            ->select('companies.company_name_th', 'announcements.announcement_title', 'resumes.*',
+                    'announcement_resumes.announcement_resume_id', 'announcement_resumes.status', 'announcement_resumes.announcement_id')
             ->get();
 
         return $announcement_resume;
