3a3e5a046fd427373511ac1a56362914d83cbe81
kaotu <kao-tu@hotmail.co.th>
2021-05-02 18:06:03 +0700

    🎨 Add field require rule 🌚

--- app/Http/RulesValidation/DashboardRules.php
+++ app/Http/RulesValidation/DashboardRules.php
@@ -6,6 +6,7 @@ trait DashboardRules
 {
     private $ruleExport = [
         'start_date' => 'required|string',
-        'end_date' => 'required|string'
+        'end_date' => 'required|string',
+        'name_reports' => 'require|array|min:1|max:3'
     ];
 }
