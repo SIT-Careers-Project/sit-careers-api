dedc5af3e9331a1f924c0875cd716649c5e9552d
kaotu <kao-tu@hotmail.co.th>
2021-05-14 02:52:13 +0700

    ✅ Update add multiple job type each case 🌚

--- tests/Unit/AnnouncementTest.php
+++ tests/Unit/AnnouncementTest.php
@@ -36,13 +36,64 @@ class AnnouncementTest extends TestCase
             'address_type' => 'announcement'
         ])->toArray();
 
-        $jobType = factory(JobType::class)->create([
+        $jobType_1 = factory(JobType::class)->create([
             'job_id' => Uuid::uuid(),
             'announcement_id' => $data['announcement_id']
-            ]);
+        ])->toArray();
 
         $data['my_user_id'] = $user['user_id'];
-        $data['job_type'] = $jobType['job_type'];
+        $data['job_type'] = [
+            $jobType_1['job_type'],
+        ];
+        $data['priority'] = '-';
+        $data['end_date'] = '2021-03-31 17:00:00';
+        $data = array_merge($data, $address);
+
+        $response = $this->postJson('api/academic-industry/announcement', $data);
+        $response->assertStatus(200);
+
+        $response_arr = json_decode($response->content(), true);
+        $announcement = Announcement::find($response_arr['announcement_id']);
+        $announcement_arr = $announcement->toArray()['announcement_id'];
+
+        $this->assertEquals($announcement_arr, $response_arr['announcement_id']);
+    }
+
+    public function test_post_announcement_with_multiple_job_type_success_should_return_announcement()
+    {
+        $data = $this->fakerAnnouncement->toArray();
+        $user = $this->fakerUser->toArray();
+        $company = $this->faker->toArray();
+
+        $address = factory(Address::class)->create([
+            'company_id' => $company['company_id'],
+            'address_type' => 'announcement'
+        ])->toArray();
+
+        $jobType_1 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $jobType_2 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'job_type' => 'Part time',
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $jobType_3 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'job_type' => 'Intern',
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $data['my_user_id'] = $user['user_id'];
+        $data['job_type'] = [
+            $jobType_1['job_type'],
+            $jobType_2['job_type'],
+            $jobType_3['job_type']
+        ];
+
         $data['priority'] = '-';
         $data['end_date'] = '2021-03-31 17:00:00';
         $data = array_merge($data, $address);
@@ -74,7 +125,9 @@ class AnnouncementTest extends TestCase
         ]);
 
         $data['my_user_id'] = $user['user_id'];
-        $data['job_type'] = $jobType['job_type'];
+        $data['job_type'] = [
+            $jobType['job_type']
+        ];
         $data['priority'] = '-';
         $data['end_date'] = '2021-03-31 17:00:00';
         $data = array_merge($data, $address);
@@ -86,7 +139,6 @@ class AnnouncementTest extends TestCase
         $response->assertStatus(200);
 
         $responseGetByID = json_decode($response->content(), true);
-
         $this->assertDatabaseHas('announcements', [
             'announcement_id' => $responseGetByID[0]['announcement_id']
         ]);
@@ -119,7 +171,9 @@ class AnnouncementTest extends TestCase
         ]);
 
         $data['my_user_id'] = $user['user_id'];
-        $data['job_type'] = $jobType['job_type'];
+        $data['job_type'] = [
+            $jobType['job_type']
+        ];
         $data['priority'] = '-';
         $data['end_date'] = '2021-03-31 17:00:00';
         $data = array_merge($data, $address);
@@ -129,7 +183,7 @@ class AnnouncementTest extends TestCase
 
         $response = $this->json('GET', 'api/academic-industry/company/announcements', ['my_user_id' => $user['user_id']]);
         $response->assertStatus(200);
-        
+
         $responseGetByID = json_decode($response->content(), true);
         $this->assertDatabaseHas('announcements', [
             'announcement_id' => $responseGetByID[0]['announcement_id']
@@ -139,32 +193,36 @@ class AnnouncementTest extends TestCase
     public function test_post_announcement_fail_should_return_error_message()
     {
         //Field announcement_title and property are missing
-        $data = [
-            'company_id' => $this->faker->company_id,
-            'job_description' => 'เป็นซอฟ์ตแวร์เอน เอนแบบเอนเตอร์เทน',
-            'job_position_id' => $this->fakerJobPosition->job_position_id,
-            'file_picture' => '',
-            'picture' => '',
-            'priority' => '-',
-            'start_date' => '2021-01-10 13:00:00',
-            'end_date' => '2021-03-31 17:00:00',
-            'salary' => '30,000',
-            'welfare' => 'เงินดี ไม่ต้องแย่งลงทะเบียน',
-            'status' => 'เปิดรับสมัคร',
-            'job_type' => 'WiL',
-            'address_one' => '138/2 พรีวิวหอพัก',
-            'address_two' => '-',
-            'lane' => '9',
-            'road' => 'วิภาวดีรังสิต',
-            'sub_district' => 'ดินแดง',
-            'district' => 'ดินแดง',
-            'province' => 'กรุงเทพ',
-            'postal_code' => '10400',
-            'start_business_day' => 'จันทร์',
-            'end_business_day' => 'ศุกร์',
-            'start_business_time' => '09:00',
-            'end_business_time' => '18:00',
+
+        $data = $this->fakerAnnouncement->toArray();
+        $user = $this->fakerUser->toArray();
+        $company = $this->faker->toArray();
+
+        $data = array_diff($data, array($data['announcement_title']));
+        $data = array_diff($data, array($data['property']));
+
+        $address = factory(Address::class)->create([
+            'company_id' => $company['company_id'],
+            'address_type' => 'announcement'
+        ])->toArray();
+
+        $jobType_1 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $jobType_2 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'job_type' => 'Part time',
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $data['my_user_id'] = $user['user_id'];
+        $data['job_type'] = [
+            $jobType_1['job_type'],
+            $jobType_2['job_type']
         ];
+        $data = array_merge($data, $address);
 
         $response = $this->postJson('api/academic-industry/announcement', $data);
         $expected = json_decode($response->content(), true);
@@ -193,7 +251,7 @@ class AnnouncementTest extends TestCase
             'address_type' => 'announcement'
         ])->toArray();
 
-        $jobType = factory(JobType::class)->create([
+        $jobType_1 = factory(JobType::class)->create([
             'job_id' => Uuid::uuid(),
             'announcement_id' => $data['announcement_id']
         ]);
@@ -201,7 +259,52 @@ class AnnouncementTest extends TestCase
         $data['my_user_id'] = $user['user_id'];
         $data['announcement_title'] = 'รับสมัครงานตำแหน่ง UX/UI';
         $data['job_description'] = 'ต้องการ UX/UI';
-        $data['job_type'] = $jobType['job_type'];
+        $data['job_type'] = [
+            $jobType_1['job_type']
+        ];
+        $data['priority'] = '-';
+        $data['end_date'] = '2021-03-31 17:00:00';
+        $data = array_merge($data, $address);
+
+        $response = $this->putJson('api/academic-industry/announcement', $data);
+        $response->assertStatus(200);
+
+        $response_arr = json_decode($response->content(), true);
+        $announcement = Announcement::find($response_arr['announcement_id']);
+        $announcement_arr = $announcement->toArray();
+
+        $this->assertEquals($announcement_arr['announcement_id'], $response_arr['announcement_id']);
+        $this->assertEquals($announcement_arr['job_description'], $response_arr['job_description']);
+    }
+
+    public function test_update_announcement_with_mutiple_job_type_success_should_return_announcement_that_has_been_updated()
+    {
+        $data = $this->fakerAnnouncement->toArray();
+        $user = $this->fakerUser->toArray();
+        $company = $this->faker->toArray();
+
+        $address = factory(Address::class)->create([
+            'company_id' => $company['company_id'],
+            'address_type' => 'announcement'
+        ])->toArray();
+
+        $jobType_1 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'announcement_id' => $data['announcement_id']
+        ]);
+
+        $jobType_2 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'announcement_id' => $data['announcement_id']
+        ]);
+
+        $data['my_user_id'] = $user['user_id'];
+        $data['announcement_title'] = 'รับสมัครงานตำแหน่ง UX/UI';
+        $data['job_description'] = 'ต้องการ UX/UI';
+        $data['job_type'] = [
+            $jobType_1['job_type'],
+            $jobType_2['job_type']
+        ];
         $data['priority'] = '-';
         $data['end_date'] = '2021-03-31 17:00:00';
         $data = array_merge($data, $address);
@@ -219,40 +322,27 @@ class AnnouncementTest extends TestCase
 
     public function test_update_announcement_fail_should_return_error_message()
     {
-        $jobType = factory(JobType::class)->create([
-            "announcement_id" => $this->fakerAnnouncement->announcement_id,
-            "job_id" => Uuid::uuid()
-        ]);
-
-        $address = factory(Address::class)->create([
-            "company_id" => $this->faker->company_id,
-        ]);
-
         //Filed announcement_id (pk), property and postal_code are missing
-        $data = [
-            'announcement_title' => 'รับสมัครงานตำแหน่ง UX/UI',
-            'job_description' => 'ต้องการ UX/UI',
-            'job_position_id' => $this->fakerJobPosition->job_position_id,
-            'file_picture' => '',
-            'picture' => '',
-            'priority' => '-',
-            'start_date' => '2021-01-10 13:00:00',
-            'end_date' => '2021-03-31 17:00:00',
-            'salary' => '30,000',
-            'welfare' => 'เงินดี ไม่ต้องแย่งลงทะเบียน',
-            'status' => 'เปิดรับสมัคร',
-            'job_type' => 'WiL',
-            'address_one' => '138/2 พรีวิวหอพัก',
-            'address_two' => '-',
-            'lane' => '9',
-            'road' => 'วิภาวดีรังสิต',
-            'sub_district' => 'ดินแดง',
-            'district' => 'ดินแดง',
-            'province' => 'กรุงเทพ',
-            'start_business_day' => 'จันทร์',
-            'end_business_day' => 'ศุกร์',
-            'start_business_time' => '09:00',
-            'end_business_time' => '18:00',
+        $data = $this->fakerAnnouncement->toArray();
+        $user = $this->fakerUser->toArray();
+        $company = $this->faker->toArray();
+
+        $jobType_1 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $jobType_2 = factory(JobType::class)->create([
+            'job_id' => Uuid::uuid(),
+            'job_type' => 'Part time',
+            'announcement_id' => $data['announcement_id']
+        ])->toArray();
+
+        $data = array_diff($data, array($data['announcement_id']));
+        $data['my_user_id'] = $user['user_id'];
+        $data['job_type'] = [
+            $jobType_1['job_type'],
+            $jobType_2['job_type']
         ];
 
         $response = $this->putJson('api/academic-industry/announcement', $data);
@@ -262,11 +352,17 @@ class AnnouncementTest extends TestCase
             "announcement_id" => [
                 "The announcement id field is required."
             ],
-            "address_id" => [
-                "The address id field is required."
+            "address_one" => [
+                "The address one field is required."
             ],
-            "property" => [
-                "The property field is required."
+            "sub_district" => [
+                "The sub district field is required."
+            ],
+            "district" => [
+                "The district field is required."
+            ],
+            "province" => [
+                "The province field is required."
             ],
             "postal_code" => [
                 "The postal code field is required."
@@ -284,12 +380,16 @@ class AnnouncementTest extends TestCase
         $jobType = factory(JobType::class)->create([
             "announcement_id" => $data['announcement_id'],
             "job_id" => Uuid::uuid()
-        ]);
+        ])->toArray();
 
         $address = factory(Address::class)->create([
             'company_id' => $this->faker->company_id,
             'address_type' => 'announcement'
         ]);
+
+        $data['job_type'] = [
+            $jobType['job_type']
+        ];
 
         $get_announcement_id = [
             'announcement_id' => $data['announcement_id']
@@ -304,11 +404,13 @@ class AnnouncementTest extends TestCase
 
     public function test_delete_announcement_by_id_fail_should_return_fail_message()
     {
+        $data['job_type'] = [];
+
         $id = [
             'announcement_id' => $this->fakerAnnouncement->announcement_id
         ];
 
-        $expected_announcement = 'Find not found announcement or job type or address or business day.';
+        $expected_announcement = 'Find not found announcement or job type or address';
 
         $response = $this->deleteJson('api/academic-industry/announcement', $id);
         $response_arr = json_decode($response->content(), true);
