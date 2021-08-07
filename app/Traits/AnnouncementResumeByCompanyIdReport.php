8c32c635d166cf70a470ab331166e316bea4796b
kaotu <kao-tu@hotmail.co.th>
2021-08-08 00:28:51 +0700

    ♿️  Improve get application for company 🌚

--- app/Traits/AnnouncementResumeByCompanyIdReport.php
+++ app/Traits/AnnouncementResumeByCompanyIdReport.php
@@ -21,17 +21,14 @@ class AnnouncementResumeByCompanyIdReport implements FromCollection, WithHeading
         $data = $this->request;
 
         $id = $data['my_user_id'];
-        $start_date = $data['start_date']." 00:00:00";
-        $end_date = $data['end_date']." 23:59:59";
 
         $company = DataOwner::where('user_id', $id)->get();
         if (!$company->isEmpty()) {
             $announcement_resumes = AnnouncementResume::join('resumes', 'resumes.resume_id', '=', 'announcement_resumes.resume_id')
                 ->join('announcements', 'announcements.announcement_id', '=', 'announcement_resumes.announcement_id')
                 ->join('companies', 'companies.company_id', '=', 'announcements.company_id')
-                ->select('announcements.announcement_title', 'announcement_resumes.*', 'resumes.*')
+                ->select('companies.company_name_th', 'announcements.announcement_title', 'announcement_resumes.*', 'resumes.*')
                 ->where('companies.company_id', '=', $company[0]->company_id)
-                ->whereBetween('companies.created_at', [$start_date, $end_date])
                 ->get()
                 ->makeHidden(['companies.company_name_th', 'resume_id', 'student_id', 'announcement_resume_id', 'announcement_id', 'path_file', 'created_at', 'deleted_at']);
 
