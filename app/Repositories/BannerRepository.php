9e8156719d236b1d12e373723998595746568261
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-08 23:04:16 +0700

    :sparkles: Update banner feature for setting date to show follow by date 🌻

--- app/Repositories/BannerRepository.php
+++ app/Repositories/BannerRepository.php
@@ -23,7 +23,22 @@ class BannerRepository implements BannerRepositoryInterface
     {
         $banner = new Banner();
         $banner->path_image = $data['path_image'] == "" ? "-": $data['path_image'];
+        $banner->date_display_start = $data['date_display_start'];
+        $banner->date_display_end = $data['date_display_end'];
         $banner->save();
+
+        return array_merge($banner->toArray());
+    }
+
+    public function updateBanner($data)
+    {
+        $banner = Banner::where('banner_id', $data['banner_id'])
+                ->first();
+        if ($banner) {
+            $banner->date_display_start = $data['date_display_start'];
+            $banner->date_display_end = $data['date_display_end'];
+            $banner->save();
+        }
 
         return array_merge($banner->toArray());
     }
